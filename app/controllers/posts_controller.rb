678fcecb1179d10999931e20911b06d00ce84c5a
Edwin Leonard Salim <edwin.salim@ukirama.com>
2022-09-28 10:46:43 +0700

    Members Only CRU feature done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @posts = Post.all
   end
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     # binding.pry
     if @post.save
       redirect_to posts_path
@@ -20,7 +21,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    set_post
+  end
+
+  def update
+    set_post
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    set_post
+    @post.destroy
+
+    redirect_to posts_path, status: :see_other
+
+  end
+
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
   def post_params
     params.require(:post).permit(:title, :content)
   end
